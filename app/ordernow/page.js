804280575679aec2c34ba0ae41116b9786c88a04
droomiweb <designer.abinabi@gmail.com>
2024-07-22 08:36:36 +0530

    22jul order page creation

---
+++ app/ordernow/page.js
@@ -0,0 +1,13 @@
+export default function Order(params) {
+    return(
+        <>
+                  <div className="flex justify-center items-center h-screen">
+                <div className="flex justify-center items-center">
+                    <h1 className="font-extrabold text-9xl">Order your faverout food now</h1>
+                        
+                </div>
+            </div>
+
+        </>
+    )
+};
